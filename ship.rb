dbe76b11b84e5505b7f48805f464b3ce1768a791
Yong Joseph Bakos <ybakos@humanoriented.com>
2019-02-12 22:05:52 -0800

    Ship: Add naive move_left and move_right.

--- ship.rb
+++ ship.rb
@@ -2,10 +2,19 @@ class Ship
 
   WIDTH = 100
   HEIGHT = 200
+  VELOCITY = 10
 
   def initialize(x, y)
     @x = 0
     @y = 0
+  end
+
+  def move_left
+    @x -= VELOCITY
+  end
+
+  def move_right
+    @x += VELOCITY
   end
 
 end
