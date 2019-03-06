83e5a46e2fe1f7c50d0c79cde746323fb8ba59d7
Yong Joseph Bakos <ybakos@humanoriented.com>
2019-03-06 09:51:06 -0800

    Main: Add questions for POODR chapter 7.

---
+++ main.rb
@@ -0,0 +1,7 @@
+=begin
+Reflecting on Principle
+1) How are Ruby modules (and similar features from other langs)
+   different from a class, in their purpose?
+2) What is LSP?
+3) Briefly describe how the template method pattern works.
+=end
